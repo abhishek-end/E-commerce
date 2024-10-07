f526053fcd306b9ddde7a0a00921d1ada7801161
abhishek-end <abhishekyadav49534@gmail.com>
2024-10-08 02:35:29 +0530

    e-commerce b abhi

--- src/features/counter copy/CounterAPI.jsx
+++
@@ -1,6 +0,0 @@
-// counterAPI.jsx
-export function fetchCount(amount = 1) {
-  return new Promise((resolve) =>
-    setTimeout(() => resolve({ data: amount }), 500)
-  );
-}
--- src/features/counter/CounterAPI.jsx
+++
@@ -1,8 +0,0 @@
-// counterAPI.jsx
-export function fetchCount() {
-  return new Promise(async (resolve) => {
-    const response = await fetch("http://localhost:8080");
-    const data = response.json();
-    resolve({ data });
-  });
-}
